97f056780020408086a3e6d899a0690813ad2653
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-03-31 06:24:17 +0800

    done

--- app/quiz/layout.jsx
+++ app/quiz/layout.jsx
@@ -174,141 +174,144 @@ export default function Example({children}) {
                     {/* Sidebar component, swap this element with another sidebar if you like */}
                     <div className="mt-5 flex h-0 flex-1 flex-col overflow-y-auto pt-1">
                         {/* User account dropdown */}
-                        {typeof localStorage !== "undefined" && !localStorage.getItem('token') && <div className={"w-full flex justify-center"}>
-                            <GoogleLogin
-                                onSuccess={credentialResponse => {
-                                    login(credentialResponse.credential);
-                                }}
-                                onError={() => {
-                                    console.log('Login Failed');
-                                }}
-                                hosted_domain={"gapp.nthu.edu.tw"}
-                                auto_select
-                                useOneTap
-                            />
-                        </div>}
-
-                        <Menu as="div" className="relative inline-block px-3 text-left">
-                            <div>
-                                <Menu.Button
-                                    className="group w-full rounded-md bg-gray-100 px-3.5 py-2 text-left text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-                  <span className="flex w-full items-center justify-between">
-                    <span className="flex min-w-0 items-center justify-between space-x-3">
-                      <img
-                          className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
-                          src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-                          alt=""
-                      />
-                      <span className="flex min-w-0 flex-1 flex-col">
-                        <span className="truncate text-sm font-medium text-gray-900">Jessy Schwarz</span>
-                        <span className="truncate text-sm text-gray-500">@jessyschwarz</span>
-                      </span>
-                    </span>
-                    <ChevronUpDownIcon
-                        className="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                    />
-                  </span>
-                                </Menu.Button>
+                        {(typeof localStorage !== "undefined" && !localStorage.getItem('token')) ?
+                            <div className={"w-full flex justify-center"}>
+                                <GoogleLogin
+                                    onSuccess={credentialResponse => {
+                                        login(credentialResponse.credential);
+                                    }}
+                                    onError={() => {
+                                        console.log('Login Failed');
+                                    }}
+                                    hosted_domain={"gapp.nthu.edu.tw"}
+                                    auto_select
+                                    useOneTap
+                                />
                             </div>
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
-                            >
-                                <Menu.Items
-                                    className="absolute left-0 right-0 z-10 mx-3 mt-1 origin-top divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    View profile
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    Settings
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    Notifications
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                    </div>
-                                    <div className="py-1">
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    Get desktop app
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    Support
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                    </div>
-                                    <div className="py-1">
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(
-                                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                        'block px-4 py-2 text-sm'
-                                                    )}
-                                                >
-                                                    Logout
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                    </div>
-                                </Menu.Items>
-                            </Transition>
-                        </Menu>
+                            :
+                            <Menu as="div" className="relative inline-block px-3 text-left">
+                                <div>
+                                    <Menu.Button
+                                        className="group w-full rounded-md bg-gray-100 px-3.5 py-2 text-left text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                                      <span className="flex w-full items-center justify-between">
+                                        <span className="flex min-w-0 items-center justify-between space-x-3">
+                                          <img
+                                              className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
+                                              src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
+                                              alt=""
+                                          />
+                                          <span className="flex min-w-0 flex-1 flex-col">
+                                            <span
+                                                className="truncate text-sm font-medium text-gray-900">Jessy Schwarz</span>
+                                            <span className="truncate text-sm text-gray-500">@jessyschwarz</span>
+                                          </span>
+                                        </span>
+                                        <ChevronUpDownIcon
+                                            className="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                                            aria-hidden="true"
+                                        />
+                                      </span>
+                                    </Menu.Button>
+                                </div>
+                                <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                >
+                                    <Menu.Items
+                                        className="absolute left-0 right-0 z-10 mx-3 mt-1 origin-top divide-y divide-gray-200 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <div className="py-1">
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        View profile
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        Settings
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        Notifications
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </div>
+                                        <div className="py-1">
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        Get desktop app
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        Support
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </div>
+                                        <div className="py-1">
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(
+                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'block px-4 py-2 text-sm'
+                                                        )}
+                                                    >
+                                                        Logout
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </Menu>
+                        }
                         {/* Sidebar Search */}
                         <div className="mt-5 px-3">
                             <label htmlFor="search" className="sr-only">
@@ -337,7 +340,7 @@ export default function Example({children}) {
                             <div className="mt-8">
                                 {/* Secondary navigation */}
                                 <h3 className="px-3 text-sm font-medium text-gray-500" id="desktop-teams-headline">
-                                    Teams
+                                    Tags
                                 </h3>
                                 <div className="mt-1 space-y-1" role="group" aria-labelledby="desktop-teams-headline">
                                     {teams.map((team) => (
